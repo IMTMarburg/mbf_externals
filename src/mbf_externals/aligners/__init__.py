26cf02f78b94278013b60a9e7cd50dc741dff756
Unknown <marco.mernberger@uni-marburg.de>
2020-09-11 08:37:40 +0200

    bwa,bbmap

--- src/mbf_externals/aligners/__init__.py
+++ src/mbf_externals/aligners/__init__.py
@@ -3,5 +3,6 @@ from .subread import Subread, Subjunc
 from .bowtie import Bowtie
 from .salmon import Salmon
 from .bwa import BWA
+from .bbmap import BBMap, ExtendCigarBBMap
 
-all = [Bowtie, Subread, STAR, Salmon, Subjunc, BWA]
+all = [Bowtie, Subread, STAR, Salmon, Subjunc, BWA, BBMap, ExtendCigarBBMap]
